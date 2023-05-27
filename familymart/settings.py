3edd84968c3edd7ee15a64d1e2908b3ae8185491
chunch3oh <56960972+chunch3oh@users.noreply.github.com>
2023-05-28 01:24:16 +0800

    Add files via upload

--- familymart/settings.py
+++ familymart/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-x#0*!2=t3_s^6ivr((4ajudqkvm)-ea)qf76sc9hxlxdz-&_hb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beourguest.herokuapp.com']
+ALLOWED_HOSTS = ['familymart.herokuapp.com']
 
 
 # Application definition
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'familymart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(Path(BASE_DIR, 'db.sqlite3')),
+        'NAME': Path(BASE_DIR, 'db.sqlite3'),
     }
 }
 
