f627e95d8007a5e8f11bba6107625a2f0c248f01
chunch3oh <56960972+chunch3oh@users.noreply.github.com>
2023-05-28 00:18:35 +0800

    Update settings.py

--- familymart/settings.py
+++ familymart/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'familymart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(Path(BASE_DIR, 'db.sqlite3')),
     }
 }
 
